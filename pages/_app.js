247bda08561578f181acdc727ebc91e85ff53f49
Hayden Tedrake <63128455+HaydenTedrake@users.noreply.github.com>
2025-01-11 13:28:06 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
+      <Head>
+        <title>hayden</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
